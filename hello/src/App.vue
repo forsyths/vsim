4b10bb89044d41f292444307da99d2434b5dc61e
Stuart Forsyth <forsyths@gmail.com>
2021-01-13 23:13:59 +0400

    test

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Mr. Forsyth welcomes you to our Vue.js hello-world app"/>
   </div>
 </template>
 
--- hello/src/components/HelloWorld.vue
+++ hello/src/components/HelloWorld.vue
@@ -27,6 +27,9 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <ul>
+      <li>Seeking understanding of all this new infrastructure..... OMG!!!!!</li>
+    </ul>
   </div>
 </template>
 
